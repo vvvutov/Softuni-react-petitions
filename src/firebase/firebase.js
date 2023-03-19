1d432d3737a1dc2d8dc273b95c7e5850672b3022
V Vutov <vladislav@vutov.org>
2023-03-19 17:30:38 +0200

    check if firebase config is git ignored

---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import  { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore} from 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD1t9usMmzBBo7TMeIFbOUgCPy8Ilg_M8w",
+  authDomain: "petitions-2f2b0.firebaseapp.com",
+  projectId: "petitions-2f2b0",
+  storageBucket: "petitions-2f2b0.appspot.com",
+  messagingSenderId: "460242636813",
+  appId: "1:460242636813:web:2abc63e98ccc7f52334544",
+  measurementId: "G-1EC893TLMM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const googleProvider = new GoogleAuthProvider();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
