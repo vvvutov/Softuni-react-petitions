9727da67769bc957b2197b96d1fd8e99eeb6118d
V Vutov <vladislav@vutov.org>
2023-03-21 18:16:13 +0200

    edit the firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  projectId: `"petitions-2f2b0"`,
   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
   messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
   appId: `${process.env.REACT_APP_APP_ID}`,
